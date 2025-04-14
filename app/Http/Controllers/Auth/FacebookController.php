4a9c12dada4032522a781fb88d985f0be0c56602
kawsar24ahmad <kawsarmojomdar@gmail.com>
2025-04-14 23:15:24 +0600

    admin and customer login and dashboard is added and profile edit photo upload is added

---
+++ app/Http/Controllers/Auth/FacebookController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+
+class FacebookController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function redirectToFacebook()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function handleFacebookCallback(Request $request)
+    {
+        $facebookUser = Socialite::driver('facebook')->user();
+        // dd( $facebookUser);
+
+
+        $user = User::where('facebook_id', $facebookUser->id)->first();
+
+        if ($user) {
+            // User exists — update other fields, but NOT password
+            $user->update([
+                'name' => $facebookUser->name,
+                'email' => $facebookUser->email,
+                'facebook_token' => $facebookUser->token,
+                'facebook_refresh_token' => $facebookUser->refreshToken,
+                'avatar' => $facebookUser->avatar_original
+
+            ]);
+        } else {
+            // New user — create with password
+            $user = User::create([
+                'name' => $facebookUser->name,
+                'email' => $facebookUser->email,
+                'password' => null,
+                'facebook_id' => $facebookUser->id,
+                'facebook_token' => $facebookUser->token,
+                'facebook_refresh_token' => $facebookUser->refreshToken,
+                'role_id'=> 1,
+                'role' => 'customer',
+                'avatar' => $facebookUser->avatar_original
+            ]);
+        }
+
+        // Auth::login($user);
+
+        // $authUser = auth()->user();
+        // dd($authUser->password);
+        if (is_null($user->password)) {
+            return redirect()->route('password.set', $user->facebook_id);
+        }
+        Auth::login($user);
+
+        return redirect('/customer')->with('success', 'You have successfully logged in with Facebook.');
+    }
+}
